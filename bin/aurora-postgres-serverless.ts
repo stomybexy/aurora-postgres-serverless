d9fa8ceaae3d0f400beca9c6aeb5eaa1f2d36f5a
Jonatan Sombié <jonatan.sombie@gmail.com>
2023-08-22 12:21:36 +0200

    deploy a basic serverless postgres on aurora

--- bin/aurora-postgres-serverless.ts
+++ bin/aurora-postgres-serverless.ts
@@ -3,4 +3,9 @@ import * as cdk from 'aws-cdk-lib';
 import { AuroraPostgresServerlessStack } from '../lib/aurora-postgres-serverless-stack';
 
 const app = new cdk.App();
-new AuroraPostgresServerlessStack(app, 'AuroraPostgresServerlessStack');
+new AuroraPostgresServerlessStack(app, 'AuroraPostgresServerlessStack', {
+    env: {
+        region: process.env.CDK_DEFAULT_REGION,
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+    }
+});
